1b3629fc515f8bb2a9b04d897d3dc2bc2fba1c8d
tanghui0616 <tanghui0616@gmail.com>
2015-07-20 14:22:14 -0400

    change makesed.py

--- makefile/makesed.py
+++ makefile/makesed.py
@@ -6,7 +6,7 @@
     from numpy import *
     def makesed():
         """
-        Output topography file for the entire domain
+        Output sediment file for the entire domain including sediment thickness and grainsize distribution
         """
         nxpoints = 2001
         nypoints = 2001
@@ -14,63 +14,49 @@
         xupper = 200000.e0
         yupper = 200000.e0
         ylower = 0
-        outfile= "sed.type2"
-        topotools.sed2writer(outfile,sed,xlower,xupper,ylower,yupper,nxpoints,nypoints)
-    def makeqinit():
-        """
-        Create qinit data file
-        """
-        nxpoints = 2001
-        nypoints = 2001
-        xlower = 0.e0
-        xupper = 200000.e0
-        yupper = 200000.e0
-        ylower = 0.e0
-        outfile= "hump.xyz"
-        topotools.sed1writer(outfile,qinit,xlower,xupper,ylower,yupper,nxpoints,nypoints)
+        outfile1= "thick.type2"
+        outfile2= "perc.type2"
+        sedtools.sed2writer(outfile1,thickness,xlower,xupper,ylower,yupper,nxpoints,nypoints)
+        sedtools.sed2writer(outfile2,perc,xlower,xupper,ylower,yupper,nxpoints,nypoints,nclass)
+    
 
-    def sed(x,y):
+
+    def thickness(x,y):
         """
-        Test case
+        Test case for sediment transport
         """
         # value of z at origin:  Try zmin = 80 for shoreline or 250 for no shore
-        from pylab import plot,show,ylim
         nxpoints = 2001
         nypoints = 2001
-        slope = 0.0275 #slope
         endpoint = 80000 #endpoint
         th = zeros(shape=shape(nxpoints,nypoints))
         for i in range(nxpoints):
             for j in range(nypoints):
                 th(i,j) = 0.5
         return th
-
-    def qinit(x,y):
+        
+    def perc(x,y):
         """
-        Gaussian hump:
+        Test case for sediment transport
         """
-        from numpy import where
-        from pylab import plot,show
+        # value of z at origin:  Try zmin = 80 for shoreline or 250 for no shore
         nxpoints = 2001
         nypoints = 2001
-        zmin = 1000.0
-        zmin1 = 1000
-        g = 9.81
-        A = 10.0 # wave height
-        k = sqrt(3*A/(4*zmin**3))
-        z = zeros(shape=shape(y))
-        u = zeros(shape=shape(y))
-        hu = zeros(shape=shape(y))
-        y0 = 180000
-        c = sqrt(g*(A+zmin))
-        rho = 1.0e3
+        nclass = 2
+        endpoint = 80000 #endpoint
+        perc = zeros(shape=shape(nxpoints,nypoints,nclass))
         for i in range(nxpoints):
             for j in range(nypoints):
-                z[i,j] = A*cosh(k*(y[i,j]-y0))**(-2)
-                u[i,j] = -sqrt(g/zmin)*z[i,j]
-                hu[i,j] =(z[i,j]+zmin)*u[i,j]
-        return hu
+                for k in range (nclass):
+                    if (i > 1000):
+                        perc(i,j,0) = 1.0
+                        perc(i,j,1) = 0.0
+                    else:
+                        perc(i,j,0) = 0.0
+                        perc(i,j,1) = 1.0
+        return perc
+
 
     if __name__=='__main__':
         makesed()
-        makeqinit()
+
